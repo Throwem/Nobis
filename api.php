a76dbc88c8eaa137bb126b98240220c0861e6cb9
Throwem <throwemmail@gmail.com>
2017-11-13 23:52:54 +0530

    initial commit

---
+++ api.php
@@ -0,0 +1,15 @@
+<?php
+	// $userData = $_POST['data'];
+	$dbname = "nobis_db";
+	$user = "root";
+	$pass = "";
+	$servername = "localhost";
+	// var_dump($userData);
+	var_dump($dbname);
+	var_dump($user);
+	var_dump($pass);
+	var_dump($servername);
+	// $con = mysqli($servername, $user, $pass,$dbname);
+	// $query = "insert into user_data (post_data) values('$userData')";
+	// mysqli_query($con,$query);
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel=icon href=assets/img/fav.png sizes="16x16" type="image/png">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <title>NOBIS | Landslide Detection And Evacuation System</title>
+    
+</head>
+<body>
+    <div class="center-logo-wrapper">
+        <img class="center-logo" src="assets/img/logo.png">
+    </div>
+    <div class="form-auth-wrapper">
+        <form class="form-auth" action="summary">
+            <input class="form-auth-user form-auth-input" type="text" placeholder="Username">
+            <input class="form-auth-pass form-auth-input" type="password" placeholder="Password">
+            <button class="form-auth-submit" type="submit">Log In</button>
+        </form>
+    </div>
+</body>
+</html>
